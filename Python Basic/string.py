2e52fac14a721c689eb10f1095c3907ef85694d8
tanzin007 <edmunddantez007@gmail.com>
2025-07-18 20:31:54 +0600

    added new code in python basic folder

---
+++ Python Basic/string.py
@@ -0,0 +1,44 @@
+# In Python, Strings are arrays of bytes representing Unicode characters. 
+
+# A string is a collection of one or more characters put in a single quote, double-quote or triple quote. 
+
+# In python there is no character data type, a character is a string of length one. 
+
+# It is represented by str class.
+
+
+# Creating String - Strings in Python can be created using single quotes or double quotes or even triple quotes.
+
+
+
+
+# Python Program for 
+# Creation of String 
+  
+# Creating a String  
+# with single Quotes 
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ") 
+print(String1) 
+  
+# Creating a String 
+# with double Quotes 
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ") 
+print(String1) 
+print(type(String1))
+  
+# Creating a String 
+# with triple Quotes 
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ") 
+print(String1) 
+print(type(String1))
+
+# Creating String with triple 
+# Quotes allows multiple lines 
+String1 = '''Geeks 
+            For 
+            Life'''
+print("\nCreating a multiline String: ") 
+print(String1)
