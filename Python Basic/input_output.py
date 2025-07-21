884611e277fc7fdbb978bf81e248531ec272ef93
tanzin007 <edmunddantez007@gmail.com>
2025-07-21 12:16:51 +0600

    input and output in python

---
+++ Python Basic/input_output.py
@@ -0,0 +1,47 @@
+# User enters the values in the Console and that value is then used in the program as it was required.
+# To take input from the user we make use of a built-in function input().
+
+# input
+input1 = input()
+
+# output
+print(input1)
+
+# -------------x----------
+
+# Typecasting the input to Integer: There might be conditions when you might require integer input from user/Console, 
+# the following code takes two input(integer/float) from console and typecasts them to integer then prints the sum.
+
+# input
+num1 = int(input())
+num2 = int(input())
+
+# printing the sum in integer
+print(num1 + num2)
+
+
+# -------------x-----------
+
+
+# Typecasting the input to Float: To convert the input to float the following code will work out.
+
+# input
+num1 = float(input())
+num2 = float(input())
+
+# printing the sum in float
+print(num1 + num2)
+
+
+# -------------x------------
+
+
+
+# Typecasting the input to String: All kind of input can be converted to string type whether they are float or integer. 
+# We make use of keyword str for typecasting. 
+
+# input
+string = str(input())
+
+# output
+print(string)
