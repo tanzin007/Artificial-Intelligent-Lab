33b8b4e927184cb8fb00570f993adae92e2f2926
tanzin007 <edmunddantez007@gmail.com>
2025-07-21 12:01:36 +0600

    implicit type conversion in python

---
+++ Python Basic/implicit_type_conversion.py
@@ -0,0 +1,16 @@
+# In Implicit type conversion of data types in Python, 
+# the Python interpreter automatically converts one data type to another without any user involvement. 
+# To get a more clear view of the topic see the below examples.
+
+x = 10
+
+print("x is of type:",type(x))
+
+y = 10.6
+print("y is of type:",type(y))
+
+z = x + y
+
+print(z)
+print("z is of type:",type(z))
+
