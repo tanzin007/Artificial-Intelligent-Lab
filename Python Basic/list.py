f4fbd39497fe8c7b5a536b39350a8b4886233e1c
tanzin007 <edmunddantez007@gmail.com>
2025-07-21 11:26:22 +0600

    list is python

---
+++ Python Basic/list.py
@@ -0,0 +1,32 @@
+# Lists are just like arrays, declared in other languages which is an ordered collection of data. 
+# It is very flexible as the items in a list do not need to be of the same type.
+
+# Lists in Python can be created by just placing the sequence inside the square brackets[].
+
+
+# Python program to demonstrate  
+# Creation of List  
+  
+# Creating a List 
+List = [] 
+print("Initial blank List: ") 
+print(List) 
+  
+# Creating a List with  
+# the use of a String 
+List = ['GeeksForGeeks'] 
+print("\nList with the use of String: ") 
+print(List) 
+  
+# Creating a List with 
+# the use of multiple values 
+List = ["Geeks", "For", "Geeks"] 
+print("\nList containing multiple values: ") 
+print(List[0])  
+print(List[2]) 
+  
+# Creating a Multi-Dimensional List 
+# (By Nesting a list inside a List) 
+List = [['Geeks', 'For'], ['Geeks']] 
+print("\nMulti-Dimensional List: ") 
+print(List)
