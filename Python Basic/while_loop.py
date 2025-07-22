82907fc9ec0e7e28566bd3ec347f9a36e5f0d231
tanzin007 <edmunddantez007@gmail.com>
2025-07-22 13:54:45 +0600

    while loop in python

---
+++ Python Basic/while_loop.py
@@ -0,0 +1,10 @@
+# while loop is used to execute a block of statements repeatedly until a given condition is satisfied. 
+# And when the condition becomes false, the line immediately after the loop in the program is executed.
+
+# Python program to illustrate while loop :
+count = 0
+while (count < 3):    
+    count = count + 1
+    print("Hello Geek")
+
+print("while loop ended")
