a521d133e857b7a67d943074c2b5ab1d926aaf3c
tanzin007 <edmunddantez007@gmail.com>
2025-07-22 13:40:06 +0600

    operators in python

---
+++ operators.py
@@ -0,0 +1,16 @@
+# Python Operators : in general are used to perform operations on values and variables. 
+# These are standard symbols used for the purpose of logical and arithmetic operations. 
+
+# Examples of Arithmetic Operator
+expr = 10 + 20 * 30
+print(expr)
+
+
+name = "Alex"
+age = 40
+
+
+if ((name == "Alex" or name =="False") and age >= 2):
+    print("Hello! Welcome.")
+else:
+    print("Good Bye!!")
